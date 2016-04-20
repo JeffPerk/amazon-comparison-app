124062936b0209b5b322424040325d9c9cff9712
Jeff Perkins <jeff.perk87@gmail.com>
2016-04-20 14:41:22 -0600

    Added alert message and fixed error

--- api/controllers/amazonCtrl.js
+++ api/controllers/amazonCtrl.js
@@ -2,14 +2,14 @@ var util = require('util');
 var OperationHelper = require('apac').OperationHelper;
 var _ = require('underscore');
 var parseServ = require('../services/parseServ.js');
-var secret = require('../../secrets.js');
+// var secret = require('../../secrets.js');
 
 module.exports = {
     getProducts: function(req, res) {
     var opHelper = new OperationHelper({
-        awsId:     secret.id,
-        awsSecret: secret.awsSecret,
-        assocId:   secret.assocId,
+        awsId:     process.env.ID,
+        awsSecret: process.env.AWSSECRET,
+        assocId:   process.env.ASSOCID,
         version:   '2013-08-01'
     });
     opHelper.execute('ItemSearch', {
